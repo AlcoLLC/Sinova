adb3940f5d5b597c1dd299026016d060888b8c99
Aytac Mehdizadə <114391044+Aytac21@users.noreply.github.com>
2025-10-21 14:34:42 +0400

    indexing

--- investorRelations/apps.py
+++ investorRelations/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class InvestorrelationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'investorRelations'
+
+    def ready(self):
+        import investorRelations.signals
